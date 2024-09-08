35dbb72a1511885c5921f9924ed05563ea5c5819
astzkkk99 <asthazhorif@gmail.com>
2024-09-08 20:00:11 +0700

    Changed models and migrated

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
-    item_type = models.CharField(max_length=255)
-    item_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="",null=False)
+    item_type = models.CharField(max_length=255, default="")
+    item_name = models.CharField(max_length=255, default="",null=False)
     item_price = models.IntegerField()
     item_description = models.TextField()
     
--- main/views.py
+++ main/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'title' : "CONSIGN!",
-        'item_type' : 'Console',
-        'item_name' : 'PS4',
+        'item_type' : "Console",
+        'item_name' : "PS4",
         'item_price': '2000000',
-        'item_description': 'PS4 bekas, udah dipake 4 tahun, minus lecet aja'
+        'item_description': "PS4 bekas, udah dipake 4 tahun, minus lecet aja"
     }
 
     return render(request, "main.html", context)
